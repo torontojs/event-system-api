ff55c0af4922254f761e2ebcee080dfb4b43e73c
Diana kagotho <dianakago2@gmail.com>
2021-08-19 15:48:23 -0400

    Airtable integration

--- dist/event/controller/item.js
+++ dist/event/controller/item.js
@@ -7,26 +7,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const controller_1 = __importDefault(require("@curveball/controller"));
 class Event extends controller_1.default {
     async get(ctx) {
-        ctx.response.body = {
-            _links: {
-                self: { href: "http://localhost:8500/event" },
-                "attendee-collection": {
-                    href: "/event/1/attendees/",
-                    title: "Event name attendee's",
+        var Airtable = require("airtable");
+        var base = new Airtable({ apiKey: "key1BPt0W7VMSQko5" }).base("appzwXHVTy5YZFalo");
+        return base("Events")
+            .find(ctx.params.id)
+            .then((record) => {
+            console.log("Retrieved", record.id);
+            ctx.response.body = {
+                _links: {
+                    self: { href: "http://localhost:8500/event" },
+                    "attendee-collection": {
+                        href: "/event/1/attendees/",
+                        title: "Event name attendee's",
+                    },
+                    "host-collection": {
+                        href: "/event/host",
+                        title: "Event name host",
+                    },
+                    "schedule-collection": "/event/schedule",
                 },
-                "host-collection": {
-                    href: "/event/host",
-                    title: "Event name host",
-                },
-                "schedule-collection": "/event/schedule",
-            },
-            name: "Event name1",
-            type: "online or in-person",
-            address: "http:// or 123 Yonge Street, Unit 456",
-            start_date: 1234567890,
-            end_date: 1234567891,
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the",
-        };
+                name: record.get("name"),
+                type: "",
+                address: "",
+                start_date: 1234567890,
+                end_date: 1234567891,
+                description: "",
+            };
+        });
     }
 }
 exports.default = Event;
--- src/event/controller/item.ts
+++ src/event/controller/item.ts
@@ -4,27 +4,36 @@ import { Context } from "@curveball/core";
 
 export default class Event extends Controller {
   async get(ctx: Context) {
-    ctx.response.body = {
-      _links: {
-        self: { href: "http://localhost:8500/event" },
-        "attendee-collection": {
-          href: "/event/1/attendees/",
-          title: "Event name attendee's",
-        },
-        "host-collection": {
-          href: "/event/host",
-          title: "Event name host",
-        },
-        "schedule-collection": "/event/schedule",
-      },
+    var Airtable = require("airtable");
+    var base = new Airtable({ apiKey: "key1BPt0W7VMSQko5" }).base(
+      "appzwXHVTy5YZFalo"
+    );
 
-      name: "Event name1",
-      type: "online or in-person",
-      address: "http:// or 123 Yonge Street, Unit 456",
-      start_date: 1234567890,
-      end_date: 1234567891,
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the",
-    };
+    return base("Events")
+      .find(ctx.params.id)
+      .then((record: any) => {
+        console.log("Retrieved", record.id);
+        ctx.response.body = {
+          _links: {
+            self: { href: "http://localhost:8500/event" },
+            "attendee-collection": {
+              href: "/event/1/attendees/",
+              title: "Event name attendee's",
+            },
+            "host-collection": {
+              href: "/event/host",
+              title: "Event name host",
+            },
+            "schedule-collection": "/event/schedule",
+          },
+
+          name: record.get("name"),
+          type: "",
+          address: "",
+          start_date: 1234567890,
+          end_date: 1234567891,
+          description: "",
+        };
+      });
   }
 }
